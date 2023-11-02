1c54709ce1bbbb7caab6a59377b070d1d6941a5b
Mahdi <147751958+mahdiali636@users.noreply.github.com>
2023-11-02 14:59:47 -0400

    Create Class1.cs

---
+++ Source/Gameplay/Fisherman Minigame/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoD_23-24.Source.Gameplay.Fisherman_Minigame
+{
+    internal class Class1
+{
+}
+}
